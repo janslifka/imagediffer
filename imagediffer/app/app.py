76a6f00f7ea3c008f4db8635bffff0abeb77ce65
Jan Slifka <jan.slifka@gmail.com>
2017-01-28 14:48:37 +0100

    Add diff creating based on settings

--- imagediffer/app/app.py
+++ imagediffer/app/app.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets, uic
 
 from .config import PATH_UI_MAINWINDOW, PATH_UI_OPEN_IMAGE
-from ..lib.differ import euclidean_distance, chebyshev_distance
+from ..lib.differ import euclidean_distance, chebyshev_distance, diff
 from ..lib.loader import load_image_from_file, load_image_from_url
 from .image_struct import ImageStruct
 
@@ -25,6 +25,7 @@ class App:
 
         self._first_image = ImageStruct()
         self._second_image = ImageStruct()
+        self._diff = None
 
     def run(self):
         self._window.show()
@@ -89,9 +90,27 @@ class App:
         self._compare_images()
 
     def _compare_images(self):
-        print(
-            'Compare images: \nmethod: {}\nmode: {}\ntolerance: {}\n'.format(self._comparison_method, self._color_mode,
-                                                                             self._tolerance))
+        if self._first_image.initialized and self._second_image.initialized:
+            self._create_diff()
+
+    def _create_diff(self):
+        if self._color_mode == 'grayscale':
+            image1 = self._first_image.grayscale
+            image2 = self._second_image.grayscale
+        elif self._color_mode == 'red':
+            image1 = self._first_image.red
+            image2 = self._second_image.red
+        elif self._color_mode == 'green':
+            image1 = self._first_image.green
+            image2 = self._second_image.green
+        elif self._color_mode == 'blue':
+            image1 = self._first_image.blue
+            image2 = self._second_image.blue
+        else:
+            image1 = self._first_image.image
+            image2 = self._second_image.image
+
+        self._diff = diff(image1, image2, self._comparison_method, self._tolerance)
 
     def _load_first_image_from_file(self):
         image = self._load_image_from_file()

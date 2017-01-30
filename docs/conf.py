90738a55a5ad748ac1b9e9e9f82553f4e809af5d
Jan Slifka <jan.slifka@gmail.com>
2017-01-30 22:08:47 +0100

    Add PyQt5 mock for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,19 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import sys
+from unittest.mock import MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
+
+MOCK_MODULES = ['PyQt5']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
